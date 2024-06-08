6f835b91b2d49ac96cc1d034116752b4cf73ed54
Chol1000 <c.monykuch@alustudent.com>
2024-06-08 09:31:20 -0700

    Display menu

--- summative/main.sh
+++ summative/main.sh
@@ -7,7 +7,7 @@ FILE="students-list_1023.txt"
 if [ ! -f $FILE ]; then
     touch $FILE
 fi
-2
+
 # Display the menu
 display_menu() {
     echo "1. Create student record"
@@ -15,4 +15,16 @@ display_menu() {
     echo "3. Delete a student record"
     echo "4. Update a student record"
     echo "5. Exit"
+}
+
+# Create a new student record
+create_student() {
+    echo "Enter student's email:"
+    read email
+    echo "Enter student's age:"
+    read age
+    echo "Enter student's ID:"
+    read id
+    echo "$email, $age, $id" >> $FILE
+    echo "Student record created successfully!"
 }
