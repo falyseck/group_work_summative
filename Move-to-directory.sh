1f97b7a93e0fa451f39222a72bdd1a8bbb90cec8
falyseck <e.seck@alustudent.com>
2024-06-13 11:48:14 +0000

    some fixing

---
+++ Move-to-directory.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIRECTORY_NAME="Negpod_id-q1"
+mkdir -p "$DIRECTORY_NAME"
+
+mv main.sh "$DIRECTORY_NAME"
+mv select-emails.sh "$DIRECTORY_NAME"
+mv student-emails.txt "$DIRECTORY_NAME"
+
